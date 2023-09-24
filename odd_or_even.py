677a3bc83e56eec1617fcfadca5fc70e4ec88b8d
ishuu19 <anayedeshan@gmail.com>
2023-09-25 02:34:22 +0800

    odd_or_even

---
+++ odd_or_even.py
@@ -0,0 +1,8 @@
+# odd or even
+
+number= int(input("Which number do you want to choose?"))
+remainder = number%2
+if remainder==1:
+    print('This is an odd number')
+else:
+    print("This is an even number")
